4a8ed201702269a41777662247c95056cbee774b
miracleonyenma <miracleficient@gmail.com>
2019-07-31 00:22:59 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
-var cont = document.querySelectorAll(".cont"), 
+var loader = document.querySelector(".loader"),
+main = document.querySelector(".main"),
+cont = document.querySelectorAll(".cont"), 
 pos = 0;
 
 function hideLoader(){
     setTimeout(function(){
         cont[0].style.transform = "var(--scale)";
         setTimeout(function(){
-            cont[0].style.opacity = "0";    
+            cont[0].style.opacity = "0"; 
+            setTimeout(function(){
+                loader.style.display = "none";
+                main.style.opacity = "1";
+            }, 500);
         }, 500);
     }, 1000);
 }
